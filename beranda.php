fef2fdffae491b2666db0416ec51e2a6eab4da42
fanizuldik <fanizuldik@gmail.com>
2012-12-03 06:01:19 +0000

    file php menampilkan beranda

---
+++ beranda.php
@@ -0,0 +1,25 @@
+<html>
+  <head>
+  <title>BUKET KUKET </title>
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet" media="screen">
+  </head>
+  <body>
+
+<div class="navbar">
+  <div class="navbar-inner">
+    <a class="brand" href="#">Title</a>
+    <ul class="nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">Buku SD</a></li>
+	  <li><a href="#">Buku SMP</a></li>
+	  <li><a href="#">Buku SMA</a></li>
+      <li><a href="#">Tentang Kami</a></li>
+    </ul>
+  </div>
+</div>
+
+
+<img src="untitled.png" width="800"/>
+
+  </body>
+</html>
