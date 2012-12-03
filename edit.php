c50234f5da21f5444eda7018d7df41de609c097c
fanizuldik <fanizuldik@gmail.com>
2012-12-03 05:53:06 +0000

    file php interface untuk edit profil

---
+++ edit.php
@@ -0,0 +1,55 @@
+<html>
+  <head>
+    <title>Ubah Profil</title>
+    
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+  </head>
+  <body>
+	
+	<label>Ubah Profil</label>
+	
+	<form class="form-horizontal" method="post" action="updateedit.php">
+	<div class="control-group">
+    <label class="control-label" for="inputNama">Nama</label>
+    <div class="controls">
+      <input type="text" id="inputNama">
+    </div>
+  </div>
+  <div class="control-group">
+    <label class="control-label" for="inputEmail">Email</label>
+    <div class="controls">
+      <input type="text" id="inputEmail">
+    </div>
+  </div>
+  <div class="control-group">
+    <label class="control-label" for="inputUsername">Username</label>
+    <div class="controls">
+      <input type="text" id="inputUsername">
+    </div>
+  </div>
+   <div class="control-group">
+    <label class="control-label" for="inputPassword">Password</label>
+    <div class="controls">
+      <input type="password" id="inputPassword">
+    </div>
+  </div>
+   <div class="control-group">
+    <label class="control-label" for="inputUlangiPassword">Ulangi Password</label>
+    <div class="controls">
+      <input type="password" id="inputUlangiPassword">
+    </div>
+  </div>
+  <div class="control-group">
+    <label class="control-label" for="inputInstansi">Instansi Pendidikan</label>
+    <div class="controls">
+      <input type="text" id="inputInstansi">
+    </div>
+  </div>
+  
+      
+      <button type="submit" class="btn">Update</button>
+    </div>
+  </div>
+</form>
+</body>
+  </html>
