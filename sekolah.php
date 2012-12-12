e50bc570fa49decfbedc042de3c93ede5ec2a17d
yanazmi.hmsi <yanazmi.hmsi@gmail.com>
2012-12-12 08:41:42 +0000

    Untuk setelah masuk dari user sekolah

---
+++ sekolah.php
@@ -0,0 +1,149 @@
+<html>
+<head> <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+
+</head>
+<div class="navbar">
+				<div class="navbar-inner">
+					<a class="brand" href="#">Home</a>
+					<ul class="nav">
+						<li class="active"><a href="#">test</a></li>
+						<li><a href="#">Buku SD</a></li>
+						<li><a href="#">Buku SMP</a></li>
+						<li><a href="#">Buku SMA</a></li>
+						<li><a href="#"></a></li>
+						<li><a href="#"></a></li>
+						<li><a href="#"></a></li>
+						<li><a href="#"></a></li>
+						<li><a href="#"></a></li>
+						<li><a href="#"></a></li>
+						<li><a href="#"></a></li>
+						<li><a href="#"></a></li>
+						<li><a href="#"></a></li>
+						<li><a href="#"></a></li>
+						<li><a href="#"></a></li>
+						<li><a href="#"></a></li>
+						<li><a href="#"></a></li>
+						<li><a href="#"></a></li>
+						<li><a href="#"></a></li>
+						<li><a href="#"></a></li>
+						<li><a href="#"></a></li>
+						
+						
+						    <div class="btn-group pull-right" >
+							<a class="btn btn-medium" href="logout.php">
+							<i class="icon-user"></i><span class="hidden-phone"> Logout</span>
+							<span class="caret"></span>
+							</a>
+							</div>
+					</ul>
+				</div>
+</div>
+
+
+
+<title>Buket Kuket</title>
+
+<body>
+	<form class="form-horizontal" method="post" action="logout.php"> 
+		<div class="modal-header">
+			<h2>Buket Kuket</h2>
+				<h5> Sistem Informasi pengadaan buku paket terintegrasi</h5>
+		</div>
+		<div class="span6 main-menu-span">
+		<div class="well nav-collapse sidebar-nav">
+					<ul class="nav nav-tabs nav-stacked main-menu">
+						<li class="nav-header hidden-tablet">Selamat Datang "xxx"</li>
+						<div class="modal-body">
+<div class="control-group">
+<label class="control-label" for="Nama Buku">Nama Buku</label>
+<div class="controls">
+<input type="text" name="Nama Buku" placeholder="Nama Buku">
+</div>
+</div>
+  
+<div class="control-group">
+<label class="control-label" for="pelajaran">Pelajaran</label>
+<div class="controls">
+<input type="text" name="Pelajaran" placeholder="Pelajaran">
+</div>
+</div>
+  
+<div class="control-group">
+<label class="control-label" for="Tingkat">Tingkat</label>
+<div class="controls">
+<input type="text" name="Tingkat" placeholder="Tingkat">
+</div>
+</div>  
+
+
+<div class="control-group">
+<label class="control-label" for="Tingkat">Jumlah</label>
+<div class="controls">
+<input type="integer" name="Jumlah" placeholder="Jumlah">
+<br></br>
+
+
+                            <div class="btn-group pull-right" >
+							<a class="btn btn-small">
+							<i class="icon-user"></i><span class="hidden-phone"> Proses</span>
+							<span class="caret"></span>
+							</a>
+							</div>
+
+</div>
+</div>
+</div>
+</div>
+  </div>
+  
+  <div class="span6 main-menu-span">
+				<div class="well nav-collapse sidebar-nav">
+					<ul class="nav nav-tabs nav-stacked main-menu">
+						<li class="nav-header hidden-tablet">"Proses Konfirmasi"</li>
+						<div class="modal-body">
+<div class="control-group">
+<label class="control-label" for="Nama Buku">Nama Buku</label>
+<div class="controls">
+<input type="text" name="Nama Buku" placeholder="Nama Buku">
+</div>
+</div>
+  
+<div class="control-group">
+<label class="control-label" for="pelajaran">Pelajaran</label>
+<div class="controls">
+<input type="text" name="Pelajaran" placeholder="Pelajaran">
+</div>
+</div>
+  
+<div class="control-group">
+<label class="control-label" for="Tingkat">Tingkat</label>
+<div class="controls">
+<input type="text" name="Tingkat" placeholder="Tingkat">
+</div>
+</div>  
+
+
+<div class="control-group">
+<label class="control-label" for="Tingkat">Jumlah</label>
+<div class="controls">
+<input type="integer" name="Jumlah" placeholder="Jumlah">
+</div>
+</div>
+
+<div class="btn-group pull-right" >
+							<a class="btn btn-small">
+							<i class="icon-user"></i><span class="hidden-phone"> Edit</span>
+							<span class="caret"></span>
+							</a>
+							</div>
+
+
+</div>
+</div>
+</div> 
+  </form>
+
+					</ul>
+				</div><!--/.well -->
+			</div><!--/span-->
