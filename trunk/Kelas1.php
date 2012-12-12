ecb524fe5964af5eeb81b0714316d8047dddfc1b
yanazmi.hmsi@gmail.com <yanazmi.hmsi@gmail.com@ec63f97c-f7c4-6e79-a114-e51a2ac74a61>
2012-12-12 07:01:28 +0000

    Untuk fungsional dari proses buku SD dan masih sampai buku matematika.

---
+++ trunk/Kelas1.php
@@ -0,0 +1,81 @@
+<html>
+<head> <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+
+</head>
+<div class="navbar">
+  <div class="navbar-inner">
+    <a class="brand" href="#">Home</a>
+    <ul class="nav">
+      <li class="active"><a href="#">test</a></li>
+      <li><a href="bukusd.php">Buku SD</a></li>
+      <li><a href="#">Buku SMP</a></li>
+      <li><a href="#">Buku SMA</a></li>
+	  <li><a href="#"></a></li>
+	  <li><a href="#"></a></li>
+	  <li><a href="#"></a></li>
+	  <li><a href="#"></a></li>
+	  <li><a href="#"></a></li>
+	  <li><a href="#"></a></li>
+	   <li><a href="#"></a></li>
+	    <li><a href="#"></a></li>
+		 <li><a href="#"></a></li>
+		  <li><a href="#"></a></li>
+		   <li><a href="#"></a></li>
+		    <li><a href="#"></a></li>
+			<li><a href="#"></a></li>
+			<li><a href="#"></a></li>
+			<li><a href="#"></a></li>
+			<li><a href="#"></a></li>
+			<li><a href="#"></a></li>
+			
+							<div class="btn-group pull-right" >
+							<a class="btn btn-medium" href="logout.php">
+							<i class="icon-user"></i><span class="hidden-phone"> Logout</span>
+							<span class="caret"></span>
+							</a>
+							</div>
+							
+</form>
+	</ul>
+  </div>
+</div>
+<title>BUKU SD</title>
+<body>
+<form class="form-horizontal" method="post" action="logincode.php"> 
+  <div class="modal-header">
+  
+  <h2 align="center">Buket Kuket</h2>
+  <h5 align="center"> Sistem Informasi pengadaan buku paket terintegrasi</h5>
+  </div>
+  
+<table class="table table-bordered">
+    <td>Matematika</td>
+	<td><div class="btn-group pull-right" >
+							<a class="btn btn-medium" href="matematika.php">
+							<i class="icon-user"></i><span class="hidden-phone">Beli</span>
+							<span class="caret"></span>
+							</a>
+							</div>
+							</td>
+    </table>
+
+	<table class="table table-bordered">
+    <td>Bahasa Indonesia</td>
+    </table>
+	
+	<table class="table table-bordered">
+    <td>Bahasa Inggris</td>
+    </table>
+	
+	<table class="table table-bordered">
+    <td>Agama Islam</td>
+    </table>
+	
+	<table class="table table-bordered">
+    <td>Pendidikan Kewarganegaraan</td>
+    </table>
+	
+  </form>
+</body>
+</html>
