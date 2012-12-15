4d49f79a82b6f7c15331cb2cf9308cb277a763ef
fanizuldik <fanizuldik@gmail.com>
2012-12-15 03:08:09 +0000

    menambah code upload gambar, cek email anggota

--- daftarcode.php
+++ daftarcode.php
@@ -1,23 +1,82 @@
-<?php  
-require_once("connect.php");   
-$conn = mysql_connect('localhost','root','') or die ('Error connecting to mysql');
-mysql_select_db('rbpl',$conn);      
-if($_POST["button"]=="Daftar")  
-{  
-    $username = $_POST["username"];  
-    $password = $_POST["password"];
-	@$pilihan = $_POST["pilihan"];
-	
-      
-    $query="INSERT INTO identitas( username, password, pilihan) 
-	VALUES( '$username', '$password', '$pilihan')";   
-    $result=mysql_query($query);
-	if($result){  
-        include 'daftar.php';
-		echo "data berhasil disimpan";		
-    }else{
-		include 'daftar.php';
-		echo "data gagal disimpan";
+<?php
+
+require_once 'koneksi.php';
+
+
+
+
+$nama		= $_REQUEST ['nama'];
+$username	= $_REQUEST['username'];
+$alamat		= $_REQUEST['alamat'];
+$no_telepon	= $_REQUEST['notelp'];
+$password	= $_REQUEST['password'];
+$foto_profil		= null;
+$pilihan	= $_REQUEST['pilihan'];
+
+$set = true;
+
+
+
+$allowed_types=array(
+    'gif',
+    'jpeg',
+    'png',
+);
+
+if (in_array($_FILES["picture"]["type"], $allowed_types) && 
+            ($_FILES["picture"]["size"] < 500000)) {
+			
+	if($_FILES["picture"]["error"] > 0) {
+		//echo '<div class="alert alert-danger">Error<button class="close" data-dismiss="alert">&times;</button></div>';
+		$set = false;
+		}
+		
+	else {
+		if (file_exists("picture/" . $_FILES["picture"]["name"])) {
+			//echo '<div class="alert alert-warning">File sudah ada<button class="close" data-dismiss="alert">&times;</button></div>';
+			move_uploaded_file($_FILES["picture"]["tmp_name"], "picture2/" . $_FILES["picture"]["name"]);
+			$foto_profil = "picture2/" . $_FILES["picture"]["name"];
+		}
+		else {
+			move_uploaded_file($_FILES["picture"]["tmp_name"], "picture/" . $_FILES["picture"]["name"]);
+			$foto_profil = "picture/" . $_FILES["picture"]["name"];
+		}
+	}
 }
-}     
+
+$db = new db_bk($host,$user,$pass,$dbnm);
+$db -> _connect();
+//cek email sudah ada apa belum 
+$sql = 'select * from edit_profil where username="'.$username.'"';
+$query = mysql_query( $sql );
+$row = mysql_fetch_row( $query );
+
+	
+if( !empty( $row[1] ) ){
+	//echo 'EMAIL SUDAH TERDAFTAR<br>';
+	//$error++;
+	$set = false;
+}
+
+if($set) {	
+	$data_user = array(
+		'nama' => $nama,
+		'username' => $username,
+		'alamat' => $alamat,
+		'notelp' => $no_telepon,
+		'password' => $password,
+		'foto_profil' => $foto_profil,
+		'pilihan' => $pilihan
+		
+	);
+	
+	$db->set_data($data_user);
+	header("location:home.php?success");
+}
+
+else {
+	header("location:signup.php?failed");
+	//echo '<div class="alert alert-danger">File tidak memenuhi syarat<button class="close" data-dismiss="alert">&times;</button></div>';
+	$set = false;
+}
 ?>
