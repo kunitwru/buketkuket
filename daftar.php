2959c09339d13a1592424828b139a091300eda18
fanizuldik <fanizuldik@gmail.com>
2012-12-15 03:05:50 +0000

    menambah form pendaftaran

--- daftar.php
+++ daftar.php
@@ -8,17 +8,39 @@
 </div>
 <body>
 <blockquote class="pull-right">
-	<p><a href="login.php"><center>Masuk</center></a></p>
+	<p><a href="home.php"><center>Masuk</center></a></p>
 </blockquote>
 	<form method="POST" action="daftarcode.php">  
 		<form class="form-horizontal">
 			<div class="container-fluid">
-				<label class="control-label" for="username">Username</label>
+				<label class="control-label" for="nama">Nama</label>
 					<div class="controls">
-						<input name="username" type="text" id="username" placeholder="Gunakan Alamat Email">
+						<input name="nama" type="text" id="nama" placeholder="Nama penerbit/Instansi">
 					</div>
 				</label>	
 			</div>
+			<div class="container-fluid">
+				<label class="control-label" for="username">Username</label>
+					<div class="controls">
+						<input name="username" type="email" id="username" placeholder="Gunakan Alamat Email">
+					</div>
+				</label>	
+			</div>
+			<div class="container-fluid">
+				<label class="control-label" for="alamat">Alamat</label>
+					<div class="controls">
+						<input name="alamat" type="text" id="alamat">
+					</div>
+				</label>	
+			</div>
+			<div class="container-fluid">
+				<label class="control-label" for="notelp">No telepon</label>
+					<div class="controls">
+						<input name="notelp" type="text" id="notelp">
+					</div>
+				</label>	
+			</div>
+			
 			<div class="container-fluid">
 				<label class="control-label" for="password">Password</label>
 					<div class="controls">
@@ -26,6 +48,9 @@
 					</div>
 				</label>	
 			</div>
+			<br/>foto
+			<br/><input name="picture" type="file">
+						
 			<div class="container-fluid">
 				<label class="control-label" for="pilihan">Pilihan</label>
 					<div class="controls">
@@ -36,6 +61,7 @@
 						</select>
 					</div>
 				</label>	
+			</div>
 			</div>
 			<div class="container-fluid">
 				<input type="submit" class="btn btn-large btn-primary" name="button" value="Daftar">   
