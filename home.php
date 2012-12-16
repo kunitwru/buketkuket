ffa09e1860fe6ce4461c0533c250d6a85d1d3140
fanizuldik <fanizuldik@gmail.com>
2012-12-16 12:48:16 +0000

    halaman beranda

---
+++ home.php
@@ -0,0 +1,86 @@
+<html>
+  <head>
+    
+  <title>BUKET KUKET </title>
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet" media="screen">
+  </head>
+  <body>
+  <div class="control-group">
+  <label class="control-label" for="inputIcon"> </label>
+  <div class="controls">
+    <div class="input-prepend" >
+      <span class="add-on"><i class="icon-search">  </i></span>
+      <input class="span3" id="inputIcon" type="text" placeholder="Pencarian">
+    </div>
+  </div>
+</div>
+  
+
+	
+
+<ul class="nav nav-pills">
+  <li class="active">
+    <a href="#">Home</a>
+  </li>
+  <li><a href="#">Buku SD</a></li>
+  <li><a href="#">Buku SMP</a></li>
+  <li><a href="#">Buku SMA</a></li>
+  <li><a href="#">Tentang Kami</a></li>
+</ul>
+
+<img src="untitled.png" width="500"/>
+<div class="span3">
+	     
+  
+	<form class="form-inline" method="post" action="login.php">
+	<label>Login </label>
+		<br>
+		<!--<input type="text" class="input-small" placeholder="Username">-->
+		
+		<div class="control-group">
+				<label class="control-label" for="inputUsername"></label>
+		<div class="control">
+				<input type="text" name="username" id="inputUsername" required="required" placeholder="Username">
+				</div>
+		<div class="control-group">
+				<label class="control-label" for="inputPassword"> </label>
+				<div class="controls">
+					<input type="password" name="password" id="inputPassword" required="required" placeholder="Password">
+				</div>
+		
+		</label>
+		<input type="submit" value="login" name="signin" class="btn"></button>
+	  
+	  </div>
+    </div>
+		
+</div>
+</div>
+</div>
+</div>
+
+<br>
+<br>
+<br>
+<br>
+
+
+<div class="span5">
+<div class="well">
+<img src = "1005.jpg""/>
+</div>
+</div>
+<div class="span5">
+<div class="well">
+<img src = "1005.jpg""/>
+</div>
+</div>
+<div class="span5">
+<div class="well">
+<img src = "1005.jpg""/>
+</div>
+</div>
+
+
+</body>
+</html>
