c62b5aa5e4220463c1e9a750474a0c46c29759f7
fanizuldik@gmail.com <fanizuldik@gmail.com@ec63f97c-f7c4-6e79-a114-e51a2ac74a61>
2012-12-29 09:48:48 +0000

    di form line 23

--- trunk/daftar.php
+++ trunk/daftar.php
@@ -1,72 +1,88 @@
 <html>
-<head> <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-</head>
+<head> 
 <title>Registrasi </title>
+</head>
+<link href="bootstrap/css/bootstrap.css" rel="stylesheet" media="screen">
 <div class="hero-unit">
   <h1><center>Registrasi </center></h1>
 </div>
 <body>
+
 <blockquote class="pull-right">
 	<p><a href="home.php"><center>Masuk</center></a></p>
 </blockquote>
-	<form method="POST" action="daftarcode.php">  
-		<form class="form-horizontal">
-			<div class="container-fluid">
-				<label class="control-label" for="nama">Nama</label>
+
+
+	
+	<div class="span5">
+	<img src = "1005.jpg"/>
+	</div>
+
+
+	<div class="span5">
+	<form name="input" method="post" action="daftarcode.php" enctype="multipart/form-data" >  
+			<div class="control-group">
+				<label class="control-label">Nama</label>
 					<div class="controls">
 						<input name="nama" type="text" id="nama" placeholder="Nama penerbit/Instansi">
 					</div>
-				</label>	
+					
 			</div>
-			<div class="container-fluid">
-				<label class="control-label" for="username">Username</label>
+			<div class="control-group">
+				<label class="control-label" >Username</label>
 					<div class="controls">
 						<input name="username" type="email" id="username" placeholder="Gunakan Alamat Email">
 					</div>
-				</label>	
+					
 			</div>
-			<div class="container-fluid">
-				<label class="control-label" for="alamat">Alamat</label>
+			<div class="control-group">
+				<label class="control-label">Alamat</label>
 					<div class="controls">
 						<input name="alamat" type="text" id="alamat">
 					</div>
-				</label>	
+					
 			</div>
-			<div class="container-fluid">
-				<label class="control-label" for="notelp">No telepon</label>
+			<div class="control-group">
+				<label class="control-label" >No telepon</label>
 					<div class="controls">
-						<input name="notelp" type="text" id="notelp">
+						<input name="no_telp" type="text" id="no_telp">
 					</div>
-				</label>	
+					
 			</div>
 			
-			<div class="container-fluid">
-				<label class="control-label" for="password">Password</label>
+			<div class="control-group">
+				<label class="control-label">Password</label>
 					<div class="controls">
 						<input name="password" type="password" id="password" placeholder="Password">
 					</div>
-				</label>	
+				
 			</div>
 			<br/>foto
-			<br/><input name="picture" type="file">
+			<br/><input name="foto_profil" type="file">
 						
-			<div class="container-fluid">
-				<label class="control-label" for="pilihan">Pilihan</label>
+			<div class="control-group">
+				<label class="control-label">Pilihan</label>
 					<div class="controls">
 						<div class="control-group">
-						<select name="pilihan">
+						<select name="akun">
 						<option value="Penerbit">Penerbit</option>
 						<option value="Sekolah">Sekolah</option>
 						</select>
 					</div>
-				</label>	
+				
 			</div>
 			</div>
 			<div class="container-fluid">
-				<input type="submit" class="btn btn-large btn-primary" name="button" value="Daftar">   
+				<input type="submit" id="simpan" class="btn btn-large btn-primary" name="simpan" value="Daftar">   
 			</div>
 		</form>
-	</form>
+	</div>
+	
+	<div class="span5">
+	<img src = "1005.jpg"/>
+	</div>
+</div>
+</div>
+
 </body>  
 </html>
