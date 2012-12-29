ed791d15e4e1cebaf62f8c5f30a17832eedb232d
fanizuldik <fanizuldik@gmail.com>
2012-12-29 09:58:21 +0000

    edit_prof >> edit profil, tampilan foto belum muncul

--- edit_db.php
+++ edit_db.php
@@ -1,32 +1,60 @@
-<?php
 
-require_once 'config.php';
-require_once 'db_indoantik.php';
-
-
-
-//$id_pengguna		= $_REQUEST['id_pengguna'];
-$email_pengguna		= $_REQUEST['email_pengguna'];
-$nama_pengguna		= $_REQUEST['nama_pengguna'];
-$no_telepon			= $_REQUEST['no_telepon'];
-$alamat_pengguna	= $_REQUEST['alamat_pengguna'];
-$kodepos			= $_REQUEST['kodepos'];
-$id_provinsi		= $_REQUEST['id_provinsi'];
-$kota				= $_REQUEST['kota'];
-$foto_profil		= null;
-$id_tipe_pengguna	= $_REQUEST['id_tipe_pengguna'];
-$password			= $_REQUEST['password'];
-
-$set = true;
+<html>
+  <head>
+    
+  <title>BUKET KUKET </title>
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet" media="screen">
+  </head>
+  <body>
 
 
+<ul class="nav nav-pills">
+  <li class="active">
+    <a href="#">Home</a>
+  </li>
+  <li><a href="#">Buku SD</a></li>
+  <li><a href="#">Buku SMP</a></li>
+  <li><a href="#">Buku SMA</a></li>
+  <li><a href="#">Tentang Kami</a></li>
+</ul>
 
-$allowed_types=array(
-    'image/gif',
-    'image/jpeg',
-    'image/png',
+<img src="untitled.png" width="500"/>
+<br>
+<br>
+<br>
+
+	<div class="span5">
+	<?php
+	include "koneksi.php";
+	
+	$tampil = "SELECT foto_profil FROM edit_profil where id=".$_GET['id'];
+				
+				$hasil = mysql_query($tampil);
+				
+				while($data = mysql_fetch_object($hasil)){
+					$photo = $data->foto_profil;
+					if ($photo != null){
+					echo "
+								<img width=\"500\" src=\"$photo\" style=\"margin-bottom:20px\"\" class=\"img-polaroid\">
+								
+					";
+					}
+					else {
+					echo "
+
+								<img width=\"500\" src=\"picture/1005.jpg\" style=\"margin-bottom:20px\"\" class=\"img-polaroid\">
+					";
+					}
+				}
+				?>
+	<?php
+	
+	
+	$allowed_types=array(
+    'gif',
+    'jpeg',
+    'png',
 );
-
 if (in_array($_FILES["picture"]["type"], $allowed_types) && 
             ($_FILES["picture"]["size"] < 500000)) {
 			
@@ -47,3 +75,83 @@ if (in_array($_FILES["picture"]["type"], $allowed_types) &&
 		}
 	}
 }
+
+
+?>
+<br/>Ganti foto
+<br/><input name="picture" type="file">
+</div>
+		
+<form method="post" action="home.php">
+<div class="span5">
+<div class="well">
+
+<div class="control-group">
+<center>
+<header> EDIT PROFIL </header> </center>
+    <label class="control-label" >Nama</label>
+    <div class="controls">
+      <input type="text" name="nama" id="inputNama">
+    </div>
+  </div>
+  <div class="control-group">
+    <label class="control-label" >Username</label>
+    <div class="controls">
+      <input type="email" name="username" id="username" placeholder="Gunakan Alamat Email">
+    </div>
+  </div>
+  <div class="control-group">
+    <label class="control-label" >Alamat</label>
+    <div class="controls">
+      <input type="text" name="alamat" id="alamat">
+    </div>
+  </div>
+   <div class="control-group">
+    <label class="control-label" >No telepon</label>
+    <div class="controls">
+      <input type="no_telp" name="no_telp" id="no_telp">
+    </div>
+  </div>
+   <div class="control-group">
+    <label class="control-label" for="inputUlangiPassword">Password</label>
+    <div class="controls">
+      <input type="password" name="password" id="Password">
+    </div>
+  </div>
+  
+      
+      <button type="submit" name = "update" class="btn">Update</button>
+	  <?php
+		include ("update.php");
+		$profil=array();
+		if(isset($_POST['update']) ){
+			$profil=array(
+				"nama"=>$_POST['nama'], 
+				"username"=>$_POST['username'],
+				"alamat"=>$_POST['alamat'],
+				"no_telp"=>$_POST['no_telp'],
+				"password"=>$_POST['password']
+				
+				
+			);
+		$update=new Update();
+		$update->insert($profil);
+		}
+	  ?>
+    </div>
+  </div>
+</form>
+
+
+<div class="span5">
+<div class="well">
+<!--<img src = "1005.jpg"/>-->
+<?php 
+include ('lihat_foto.php');
+?>
+</div>
+</div>
+
+
+</body>
+</html>
