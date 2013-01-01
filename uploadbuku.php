6a501ad96625e442fd550c63d05510b77975cf52
yanazmi.hmsi <yanazmi.hmsi@gmail.com>
2013-01-01 14:53:23 +0000

    Upload buku ini nantinya akan digunakan oleh user penerbit untuk melakukan upload informasi buku.

---
+++ uploadbuku.php
@@ -0,0 +1,113 @@
+<html>
+  <head>
+  <title>BUKET KUKET </title>
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+  </head>
+  
+
+<div class="navbar">
+			<div class="navbar-inner">
+				<a class="brand" href="home.php">Home</a>
+				<ul class="nav">
+					<li><a href="bukusd.php">Buku SD</a></li>
+					<li><a href="bukusmp.php">Buku SMP</a></li>
+					<li><a href="#">Buku SMA</a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					
+			
+							<div class="btn-group pull-right" >
+								<a class="btn btn-medium" href="logout.php">
+									<i class="icon-user"></i><span class="hidden-phone"> Logout</span>
+									<span class="caret"></span>
+								</a>
+							</div>
+							
+	
+				</ul>
+				</div>
+				</div>
+<body>
+<div class="row">
+<div class="span12">
+<div class="row">
+<div class="span6 offset1">
+  <form>
+  <fieldset>
+    <legend>Inputkan Buku</legend>
+    <label>Nama Buku</label>
+    <input type="text" placeholder="Nama Buku">
+	<label>Mata Pelajaran</label>
+    <input type="text" placeholder="Mata Pelajaran">
+	<label>Jumlah Buku</label>
+    <input type="text" placeholder="Jumlah">
+	<label>Tingkat Pendidikan</label>
+	 <select>
+	 <option>Kelas 1 SD</option>
+	 <option>Kelas 2 SD</option>
+     <option>Kelas 3 SD</option>
+	 <option>Kelas 4 SD</option>
+	 <option>Kelas 5 SD</option>
+     <option>Kelas 6 SD</option>
+	 <option>Kelas 1 SMP</option>
+	 <option>Kelas 2 SMP</option>
+     <option>Kelas 3 SMP</option>
+	 <option>Kelas 1 SMA</option>
+	 <option>Kelas 2 SMA</option>
+     <option>Kelas 3 SMA</option>
+	 </select>
+	<label>Keterangan<label>
+	<textarea rows="3"></textarea>
+	<!--<label>Keterangan</label>
+    <input type="text" placeholder="Keterangan">-->
+	<div class="form-actions">
+  <button type="submit" class="btn btn-primary">Save changes</button>
+  <button type="button" class="btn">Cancel</button>
+  </div>
+</div>
+<div class="span3">Level 2</div>
+
+  </fieldset>
+</form>
