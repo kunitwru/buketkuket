049ea460fa11524a67040670249632bf4f61a748
yanazmi.hmsi@gmail.com <yanazmi.hmsi@gmail.com@ec63f97c-f7c4-6e79-a114-e51a2ac74a61>
2013-01-04 02:36:47 +0000

    Mengupload halaman untuk action login dari akun sekolah.

---
+++ trunk/homesekolah.php
@@ -0,0 +1,123 @@
+<html>
+  <head>
+  <title>BUKET KUKET </title>
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+  </head>
+  
+
+<div class="navbar navbar-inverse">
+			<div class="navbar-inner">
+				<a class="brand" href="homesekolah.php">Home</a>
+				<ul class="nav">
+					<li><a href="bukusd.php">Buku SD</a></li>
+					<li><a href="bukusmp.php">Buku SMP</a></li>
+					<li><a href="#">Buku SMA</a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					
+			
+							<div class="btn-group pull-right" >
+								<a class="btn btn-medium" href="logout.php">
+									<i class="icon-user"></i><span class="hidden-phone"> Logout</span>
+									<span class="caret"></span>
+								</a>
+							</div>
+							
+	
+				</ul>
+				</div>
+				</div><body background="2.jpg">
+<div class="row">
+ <div class="span12">
+  <div class="hero-unit">
+   <h1>BUKET KUKET</h1>
+    <h3>Siste Informasi Buku Paket Terintegrasi</h3>
+	</div>
+   </div>
+   
+   
+
+ <div class="span4">
+   <h5>Silahkan Lakukan Operasional Anda</h5>
+    <p>Sistem Informasi Buku Paket Terintegrasi</p>
+    <div class="btn-group" >
+	
+								<a class="btn btn-small" href="uploadbuku1.php">
+									<i class="icon-share"></i>Cek Profil
+									
+								</a>
+							</div>
+	</div>							
+	<!--<button class="btn btn-small btn-primary" type="button" href="uploadbuku.php">Upload Buku</button>-->
+	</div>
+   </div>
+   </div>
+   
+   <div class ="span4">
+	<h2>Selamat Datang</h2>
+	<p>Silahkan lakukan operasional dalam website ini</p>
+	</div>
+	</div>
+	
+	<div class ="span4">
+	<h2>Info Buku</h2>
+	<p>Disini anda bisa melihat list buku yang ada</p>
+	</div>
+	</div>
+	
+	<div class ="span4">
+	<h2>Info Buku</h2>
+	<p>Disini anda bisa melihat list buku yang ada</p>
+	</div>
+	
+	<div class ="span4">
+	<h2>Info Buku</h2>
+	<p>Disini anda bisa melihat list buku yang ada</p>
+	</div>
+	
+	</div>
+	</form>
+  </body>
+</html>
