5501a0face6d4247d6789cc7eaa79109c3c4b260
yanazmi.hmsi@gmail.com <yanazmi.hmsi@gmail.com@ec63f97c-f7c4-6e79-a114-e51a2ac74a61>
2013-01-04 02:38:44 +0000

    Mengupload file buku sd untuk penerbit, sehingga dikorelasikan dengan masing masing kelas yang hanya untuk view dari buku yang ada di sistem.

---
+++ trunk/bukusdpenerbit.php
@@ -0,0 +1,108 @@
+<html>
+<head> <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+
+</head>
+<div class="navbar">
+			<div class="navbar-inner">
+				<a class="brand" href="home.php">Home</a>
+				<ul class="nav">
+					<li><a href="bukusd.php">Buku SD</a></li>
+					<li><a href="bukusmp.php">Buku SMP</a></li>
+					<li><a href="#">Buku SMA</a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					
+			
+							<div class="btn-group pull-right" >
+								<a class="btn btn-medium" href="logout.php">
+									<i class="icon-user"></i><span class="hidden-phone"> Logout</span>
+									<span class="caret"></span>
+								</a>
+							</div>
+							
+	
+				</ul>
+				</div>
+				</div>
+</form>
+	</ul>
+  </div>
+</div>
+<title>BUKU SD</title>
+<body background="read.jpg">
+<form class="form-horizontal" method="post" action="logincode.php"> 
+  <div class="modal-header">
+  
+  <h2 align="center">Buket Kuket</h2>
+  <h5 align="center"> Sistem Informasi pengadaan buku paket terintegrasi</h5>
+  </div>
+  
+<table class="table table-bordered">
+    <td><a href="kelas1penerbit.php">Buku Kelas 1 SD</a></td>
+    </table>
+
+	<table class="table table-bordered">
+    <td><a href="kelas2.php">Buku Kelas 2 SD</a></td>
+    </table>
+	
+	<table class="table table-bordered">
+    <td><a href="kelas3.php">Buku Kelas 3 SD</a></td>
+    </table>
+	
+	<table class="table table-bordered">
+    <td><a href="kelas4.php">Buku Kelas 4 SD</a></td>
+    </table>
+	
+	<table class="table table-bordered">
+    <td><a href="kelas5.php">Buku Kelas 5 SD</a></td>
+    </table>
+	
+	<table class="table table-bordered">
+    <td><a href="kelas6.php">Buku Kelas 6 SD</a></td>
+    </table>
+  </form>
+</body>
+</html>
