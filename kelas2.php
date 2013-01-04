adb3d28210e9fcb9e57c315e1a84551ec0ac0dec
yanazmi.hmsi <yanazmi.hmsi@gmail.com>
2013-01-04 03:47:54 +0000

    Menguplad file untuk kelas 2 dimana dapat diakses oleh akun sekolah.

---
+++ kelas2.php
@@ -0,0 +1,141 @@
+<html>
+<head> <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+
+</head>
+<div class="navbar">
+			<div class="navbar-inner">
+				<a class="brand" href="home.php">Home</a>
+				<ul class="nav">
+					<li><a href="bukusd.php">Buku SD</a></li>
+					<li><a href="bukusmp.php">Buku SMP</a></li>
+					<li><a href="#">Buku SMA</a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					
+			
+							<div class="btn-group pull-right" >
+								<a class="btn btn-medium" href="logout.php">
+									<i class="icon-user"></i><span class="hidden-phone"> Logout</span>
+									<span class="caret"></span>
+								</a>
+							</div>
+							
+	
+				</ul>
+				</div>
+				</div>
+							
+</form>
+	</ul>
+  </div>
+</div>
+<title>BUKU SD</title>
+<body>
+<form class="form-horizontal" method="post" action="logincode.php"> 
+  <div class="modal-header">
+  
+  <h2 align="center">Buket Kuket</h2>
+  <h5 align="center"> Sistem Informasi pengadaan buku paket terintegrasi</h5>
+  </div>
+  
+<table class="table table-bordered">
+    <td>Matematika</td>
+	<td><div class="btn-group pull-right" >
+							<a class="btn btn-medium" href="matematika2.php">
+							<i class="icon-user"></i><span class="hidden-phone">Beli</span>
+							<span class="caret"></span>
+							</a>
+							</div>
+							</td>
+    </table>
+
+	<table class="table table-bordered">
+    <td>Bahasa Indonesia</td>
+	<td><div class="btn-group pull-right" >
+							<a class="btn btn-medium" href="bahasaindonesia2.php">
+							<i class="icon-user"></i><span class="hidden-phone">Beli</span>
+							<span class="caret"></span>
+							</a>
+							</div>
+							</td>
+    </table>
+	
+	<table class="table table-bordered">
+    <td>Bahasa Inggris</td>
+	<td><div class="btn-group pull-right" >
+							<a class="btn btn-medium" href="bahasainggris2.php">
+							<i class="icon-user"></i><span class="hidden-phone">Beli</span>
+							<span class="caret"></span>
+							</a>
+							</div>
+							</td>
+    </table>
+	
+	<table class="table table-bordered">
+    <td>Agama Islam</td>
+	<td><div class="btn-group pull-right" >
+							<a class="btn btn-medium" href="agamaislam2.php">
+							<i class="icon-user"></i><span class="hidden-phone">Beli</span>
+							<span class="caret"></span>
+							</a>
+							</div>
+							</td>
+    </table>
+	
+	<table class="table table-bordered">
+    <td>Pendidikan Kewarganegaraan</td>
+	<td><div class="btn-group pull-right" >
+							<a class="btn btn-medium" href="kwn2.php">
+							<i class="icon-user"></i><span class="hidden-phone">Beli</span>
+							<span class="caret"></span>
+							</a>
+							</div>
+							</td>
+    </table>
+	
+  </form>
+</body>
+</html>
