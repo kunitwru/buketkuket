a5648192d6f6931fb64d2c9e0a033877c025416f
febriari92 <febriari92@gmail.com>
2013-01-06 14:02:41 +0000

    order proses sudah bisa masuk database

--- proses.php
+++ proses.php
@@ -1,4 +1,4 @@
-html xmlns="http://www.w3.org/1999/xhtml">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <title>Buket Kuket</title>
@@ -7,31 +7,40 @@ html xmlns="http://www.w3.org/1999/xhtml">
 </head>
 <body>
 <?php
-// mengambil data yang telah di submit
+$user = 'root';
+$host = 'localhost';
+$link = mysql_connect($host, $user,'');
+if (!$link) {
+die ('tidak connect ni:' . mysql_error());
+}
+mysql_select_db('coba', $link);
 $nama = $_POST['nama'];
 $alamat = $_POST['alamat'];
+$email = $_POST ['email'];
 $buku = $_POST['buku'];
 $jumlah = $_POST['jumlah'];
- 
-// koneksi ke database
-mysql_connect('localhost', 'root', '');
-// pilih database
-mysql_select_db('bookstore');
-// memasukkan data ke dalam tabel orders
-mysql_query("INSERT INTO orders (order_name, order_address, book_id, order_amount) 
-             VALUES ('$nama', '$alamat', '$buku', '$jumlah')");
- 
-// mengambil data dari tabel books
-$result = mysql_query("SELECT * FROM books WHERE book_id = '$buku'");
-$data = mysql_fetch_array($result);
-$total = $jumlah * $data['book_price'];
-// menampilkan konfirmasi order
-echo "<h2>Terima kasih !!</h2>";
-echo "<p>Anda telah berhasil melakukan pembelian, berikut data pembelian Anda:</p>";
+/**$ID_Lama = mysql_fetch_array(mysql_query("SELECT MAX(id_order)+1 FROM order"));
+$ID=$ID_Lama [0];
+
+$lama = mysql_result(mysql_query("SELECT MAX(id_order) FROM order"),0);
+if ($lama == 0){
+	$id_or = 101;
+} else {
+	$id_or = $lama+1;
+} **/
+
+mysql_query("INSERT INTO `order` (nama_order, alamat_order, email_order, id_buku, jumlah_order) VALUES ('$nama', '$alamat', '$email', '$buku', '$jumlah')");
+$result = mysql_query("SELECT * FROM buku WHERE id_buku = '$buku'");
+$data = mysql_fetch_array($result);		
+$total = $jumlah * $data['harga'];
+
+echo "<h2>Terima Kasih</h2>";
+echo "<p>Anda telah berhasil</p>";
 echo "<p>Nama : $nama</p>";
 echo "<p>Alamat : $alamat</p>";
-echo "<p>Buku : $data[book_name]</p>";
-echo "<p>Harga : $jumlah x $data[book_price] = $total</p>";
+echo "<p>Email : $email</p>";
+echo "<p>Buku : $data[nama_buku]</p>";
+echo "<p>Harga : $jumlah x Rp. $data[harga] = Rp. $total</p>";
 ?>
 </body>
 </html>
