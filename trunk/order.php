dc82ef825f7762f1d13de1151ea44eeb124e6af1
febriari92@gmail.com <febriari92@gmail.com@ec63f97c-f7c4-6e79-a114-e51a2ac74a61>
2013-01-06 14:01:32 +0000

    menambah field inputan

--- trunk/order.php
+++ trunk/order.php
@@ -8,39 +8,44 @@
 <body>
 <h1>Order</h1>
 <form method="post" action="order_process.php">
-	<p>
-		Nama: <br />
-		<input type="text" name="nama">
-	</p>
-	<p>
-		Alamat: <br />
-		<textarea name="alamat"></textarea>
-	</p>	
-	<p>
-		Buku: <br />
-		<select name="buku">
-		<?php
-		$user = 'root';
-		$host = 'localhost';
-		$link = mysql_connect($host, $user,'');
-		if (!$link) {
-		die ('tidak connect ni:' . mysql_error());
-		}
-		mysql_select_db('rbpl');
-		$result = mysql_query("SELECT * FROM buku");
-		while($data = mysql_fetch_array($result)){
-			echo "<option value='$data[id_buku]'>$data[nama_buku] : Rp. $data[harga_buku]</option>";
-		}
-		?>
-		</select>
-	</p>
-	<p>
-		Jumlah: <br />
-		<input type="text" name="jumlah" size="5">
-	</p>
-	<p>
-		<input type="submit" value="beli" class="button">
-	</p>
+<div class="well">
+    <ul class="nav nav-tabs">
+      <li class="active"><a data-toggle="tab">Konfirmasi</a></li>
+      <li><a href="#Konfirmasi" data-toggle="tab">BuketKuket</a></li>
+    </ul>
+    <div id="myTabContent" class="tab-content">
+      <div class="tab-pane active in" id="home">
+            <label>Nama</label>
+            <input type="text" name="nama" class="input-xlarge">
+            <label>Alamat</label>
+            <textarea "rows="6" name="alamat" class="input-xlarge">
+            </textarea>
+            <label>Email</label>
+            <input type="text"  name="email" class="input-xlarge">
+			<label>Buku</label>
+			<select name="buku">
+			<?php
+			$user = 'root';
+			$host = 'localhost';
+			$link = mysql_connect($host, $user,'');
+			if (!$link) {
+			die ('tidak connect ni:' . mysql_error());
+			}
+			mysql_select_db('coba');
+			$result = mysql_query("SELECT * FROM buku");
+			while($data = mysql_fetch_array($result)){
+				echo "<option value='$data[id_buku]'>$data[nama_buku] : Rp. $data[harga]</option>";
+			}
+			?>
+			</select>
+            <label>Jumlah Pembelian</label>
+            <input type="text"  name="jumlah" class="input-xlarge">
+          	<div>
+        	    <button type="submit" class="btn btn-primary">Beli</button> 
+        	</div>
+        </form>
+      </div>
+  </div>	
 </form>	
 </body>
 </html>
