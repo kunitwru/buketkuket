c5bc6d24b848eb57bcb855399aae847a5e8016aa
fanizuldik@gmail.com <fanizuldik@gmail.com@ec63f97c-f7c4-6e79-a114-e51a2ac74a61>
2013-01-10 16:40:42 +0000

    sudah oop

--- trunk/proses.php
+++ trunk/proses.php
@@ -9,16 +9,27 @@
 <?php
 $user = 'root';
 $host = 'localhost';
-$link = mysql_connect($host, $user,'');
-if (!$link) {
-die ('tidak connect ni:' . mysql_error());
-}
-mysql_select_db('coba', $link);
+$dbnm = 'coba';
+$pass = '';
+include "kelas_order.php";
+
+$db = new kelas_order($host, $user, $pass, $dbnm);
+
 $nama = $_POST['nama'];
 $alamat = $_POST['alamat'];
 $email = $_POST ['email'];
 $buku = $_POST['buku'];
 $jumlah = $_POST['jumlah'];
+$aktivitas = array (
+	'nama' => $nama,
+	'alamat' => $alamat,
+	'email' => $email,
+	'buku' => $buku,
+	'jumlah' => $jumlah
+	);
+	
+
+
 /**$ID_Lama = mysql_fetch_array(mysql_query("SELECT MAX(id_order)+1 FROM order"));
 $ID=$ID_Lama [0];
 
@@ -28,9 +39,13 @@ if ($lama == 0){
 } else {
 	$id_or = $lama+1;
 } **/
+//aktifitas sekolah
+//mysql_query("INSERT INTO `order` (nama_order, alamat_order, email_order, id_buku, jumlah_order) VALUES ('$nama', '$alamat', '$email', '$buku', '$jumlah')");
 
-mysql_query("INSERT INTO `order` (nama_order, alamat_order, email_order, id_buku, jumlah_order) VALUES ('$nama', '$alamat', '$email', '$buku', '$jumlah')");
-$result = mysql_query("SELECT * FROM buku WHERE id_buku = '$buku'");
+$db -> create_akt_sekolah($aktivitas);
+
+$result = $db->get_totalharga ($buku);
+//$result = mysql_query("SELECT * FROM buku WHERE id_buku = '$buku'");
 $data = mysql_fetch_array($result);		
 $total = $jumlah * $data['harga'];
 
@@ -41,10 +56,14 @@ echo "<p>Alamat : $alamat</p>";
 echo "<p>Email : $email</p>";
 echo "<p>Buku : $data[nama_buku]</p>";
 echo "<p>Harga : $jumlah x Rp. $data[harga] = Rp. $total</p>";
+//kurangi stok, pembelian
 
-$stock = mysql_fetch_array(mysql_query("SELECT(`buku`.`jumlah` - `order`.`jumlah_order`) FROM `buku`,`order` WHERE `buku`.`id_buku`='$buku' AND `order`.`id_buku`='$buku'"));
+//$stock = mysql_fetch_array(mysql_query("SELECT(`buku`.`jumlah` - `order`.`jumlah_order`) FROM `buku`,`order` WHERE `buku`.`id_buku`='$buku' AND `order`.`id_buku`='$buku'"));
+$stock = $db->kurangi_buku ($buku);
+
 $stock_baru=$stock[0];
-mysql_query("UPDATE buku SET jumlah = '$stock_baru' WHERE `buku`.`id_buku`='$buku'");
+//mysql_query("UPDATE buku SET jumlah = '$stock_baru' WHERE `buku`.`id_buku`='$buku'");
+$db->update_stok($stock_baru, $buku);
 
 ?>
 </body>
